0d53feea1131a3687be10d3b8589f43732f43ba5
Zhambyl Maksotov <zhambyl.maksotov@nu.edu.kz>
2026-04-07 16:44:22 +0500

    Update zkp.py

--- crypto/zkp.py
+++ crypto/zkp.py
@@ -80,3 +80,33 @@ def prove_ballot(
         s1 = (k1 - c1 * randomness) % Q
 
         return BallotProof(a0=a0, b0=b0, a1=a1, b1=b1, c0=c0_sim, c1=c1, r0=s0_sim, r1=s1)
+
+
+def verify_ballot_proof(
+    proof: BallotProof,
+    ct: Ciphertext,
+    public_key: int,
+    election_id: str,
+) -> bool:
+    G_inv   = group_inv(G)
+    C2_mod1 = ct.c2 * G_inv % P
+
+    challenge = hash_to_scalar(
+        G, public_key, ct.c1, ct.c2,
+        proof.a0, proof.b0, proof.a1, proof.b1,
+        election_id,
+    )
+    if (proof.c0 + proof.c1) % Q != challenge:
+        return False
+
+    if group_mul(group_exp(G, proof.r0), group_exp(ct.c1, proof.c0)) != proof.a0:
+        return False
+    if group_mul(group_exp(public_key, proof.r0), group_exp(ct.c2, proof.c0)) != proof.b0:
+        return False
+
+    if group_mul(group_exp(G, proof.r1), group_exp(ct.c1, proof.c1)) != proof.a1:
+        return False
+    if group_mul(group_exp(public_key, proof.r1), group_exp(C2_mod1, proof.c1)) != proof.b1:
+        return False
+
+    return True
